b5f7cc551ad4735bf41b4351051a5cb0aa1b3cbe
ikerbm <79541602+ikerbm@users.noreply.github.com>
2024-04-27 11:26:40 -0500

    modificación test.py

--- test.py
+++ test.py
@@ -1,14 +1,34 @@
-from pymongo.mongo_client import MongoClient
+from flask import Flask, render_template
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
+app = Flask(__name__)
+
+# MongoDB setup
 uri = "mongodb+srv://crudistribuidos:eSbSo25tgf15pT49@clusterd.fb0ybuu.mongodb.net/?retryWrites=true&w=majority&appName=ClusterD"
-
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+db = client['sample_airbnb']
+collection = db['listingsAndReviews']
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+@app.route('/')
+def index():
+    # Obtener los primeros diez documentos de la colección
+    data_from_db = collection.find().limit(10)
+
+    # Filtrar los campos que se mostrarán en la página web
+    filtered_data = []
+    for document in data_from_db:
+        filtered_document = {
+            "_id": document["_id"],
+            "listing_url": document.get("listing_url", ""),
+            "name": document.get("name", ""),
+            "summary": document.get("summary", ""),
+            "property_type": document.get("property_type", "")
+        }
+        filtered_data.append(filtered_document)
+    
+    # Renderizar la plantilla HTML con los datos filtrados obtenidos de la base de datos
+    return render_template('index.html', data=filtered_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
